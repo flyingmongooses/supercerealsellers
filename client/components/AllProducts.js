b4370ea934bc0a14cd14725ba901d952ab217159
paulluhrsen <paulluhrsen@gmail.com>
2020-01-14 15:24:26 -0600

    all product compoent thunk reducer and action creaters made

---
+++ client/components/AllProducts.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+/**
+ * COMPONENT
+ */
+class AllProducts extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>list of all products</h3>
+      </div>
+    )
+  }
+}
+
+export default AllProducts
