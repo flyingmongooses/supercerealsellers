2a5606ed6846147389a13a6341b700558e3adb36
Paul L <paulluhrsen@gmail.com>
2020-01-17 15:30:06 -0600

    Update AllProducts.js

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -10,10 +10,6 @@ import Dropdown from 'react-dropdown'
 
 const options = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
 
-let rate = 0
-let rateCount = 0
-let displayRate
-
 /**
  * COMPONENT
  */
