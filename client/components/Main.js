c6995b22ac21ba391fa220202c46e0efa15b8599
Mike Stanton <mstanton779@gmail.com>
2020-01-21 14:54:54 -0600

    users wont see home if they are logged in already

--- client/components/Main.js
+++ client/components/Main.js
@@ -5,45 +5,59 @@ import {connect} from 'react-redux'
 import {postCurrentUser} from '../store/currentUser'
 
 import './styles/Main.css'
+import userReducer from '../store/user'
 class Main extends React.Component {
   render() {
-    return (
-      <div id="main">
-        <h1>Welcome to Super Cereal Sellers!</h1>
-        <button type="button">
-          <Link to="/login">Login</Link>
-        </button>
-        <button type="button">
-          <Link to="/create-account">Sign Up</Link>
-        </button>
-        <p>or</p>
-        <button
-          type="button"
-          onClick={() => {
-            console.log('button')
-            this.props.postUser({
-              firstName: 'Guest',
-              lastName: 'Guest',
-              email: 'guest@gmail.com',
-              password: 'guest',
-              address: '100 Main',
-              city: 'Chicago',
-              state: 'IL',
-              zipcode: '60622',
-              role: 'guest'
-            })
-          }}
-        >
-          <Link to="#">Continue as a Guest</Link>
-        </button>
-      </div>
-    )
+    if (!this.props.user) {
+      return (
+        <div id="main">
+          <h1>Welcome to Super Cereal Sellers!</h1>
+          <button type="button">
+            <Link to="/login">Login</Link>
+          </button>
+          <button type="button">
+            <Link to="/create-account">Sign Up</Link>
+          </button>
+          <p>or</p>
+          <button
+            type="button"
+            onClick={() => {
+              console.log('button')
+              this.props.postUser({
+                firstName: 'Guest',
+                lastName: 'Guest',
+                email: 'guest@gmail.com',
+                password: 'guest',
+                address: '100 Main',
+                city: 'Chicago',
+                state: 'IL',
+                zipcode: '60622',
+                role: 'guest'
+              })
+            }}
+          >
+            <Link to="#">Continue as a Guest</Link>
+          </button>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <div>You're already logged in</div>
+          <Link to="/products">See all products</Link>
+        </div>
+      )
+    }
   }
 }
-
+const mapState = state => {
+  return {
+    user: state.currentUser
+  }
+}
 const mapDispatch = dispatch => {
   return {
     postUser: data => dispatch(postCurrentUser(data))
   }
 }
-export default connect(null, mapDispatch)(Main)
+export default connect(mapState, mapDispatch)(Main)
